f00b09340a90dafc4c0f02548664705dfdaf70f1
aodiwei <adwdavid@163.com>
2016-09-17 23:11:40 +0800

    尝试material desigen,不太好用

---
+++ material-web/app1/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by asus on 2016/9/17.
+ */
+'use strict';
+//node_modules
+//require('angular');
+//require('angular-route');
+//require('angular-animate');
+//require('angular-aria');
+//require('angular-material');
+//require('angular-messages');
+
+require('./app.css');
+require('./app');
+
+//require('../node_modules/angular-material/angular-material.min.css');
+//require('../node_modules/angular-material/angular-material.min.js');
