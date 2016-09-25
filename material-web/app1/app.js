b19b087e06cb5d3a1075497b506385ceb215dbcf
aodiwei <adwdavid@163.com>
2016-09-26 01:08:40 +0800

    11

--- material-web/app1/app.js
+++ material-web/app1/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by asus on 2016/9/17.
  */
-var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons', 'ui.router']);
+var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons', 'ui.router', 'ngMessages']);
 
 app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog', function ($scope, $mdBottomSheet, $mdSidenav, $mdDialog) {
     $scope.toggleSidenav = function (menuId) {
@@ -147,8 +147,8 @@ app.directive('userAvatar', function () {
 });
 
 app.config(function ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
-    var customBlueMap = $mdThemingProvider.extendPalette('green', {
-        'contrastDefaultColor': 'light',
+    var customBlueMap = $mdThemingProvider.extendPalette('teal', {
+        //'contrastDefaultColor': 'light',
         'contrastDarkColors': ['50'],
         '50': 'ffffff'
     });
@@ -158,9 +158,9 @@ app.config(function ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
             'default': '500',
             'hue-1': '50'
         })
-        .accentPalette('pink');
-    $mdThemingProvider.theme('input', 'default')
-        .primaryPalette('brown');
+    //    .accentPalette('pink');
+    //$mdThemingProvider.theme('input', 'default')
+    //    .primaryPalette('brown');
 
     //
     // For any unmatched url, redirect to /state1
@@ -180,4 +180,38 @@ app.config(function ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
             }
         })
 
+});
+
+app.controller("formTabsCtrl", function ($scope) {
+    $scope.tabs = [
+        {title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
+        {title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
+        {title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
+        {title: 'Four', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
+        {title: 'Five', content: "If you remove a tab, it will try to select a new one."},
+        {title: 'Six', content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
+        {
+            title: 'Seven',
+            content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."
+        },
+        {
+            title: 'Eight',
+            content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"
+        },
+        {title: 'Nine', content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
+        {title: 'Ten', content: "If you're still reading this, you should just go check out the API docs for tabs!"}
+    ];
+
+    $scope.user = {
+       title: 'Developer',
+       email: 'ipsum@lorem.com',
+       firstName: '',
+       lastName: '',
+       company: 'Google',
+       address: '1600 Amphitheatre Pkwy',
+       city: 'Mountain View',
+       state: 'CA',
+       biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+       postalCode: '94043'
+     };
 });
