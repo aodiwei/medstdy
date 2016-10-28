63fd7182310c55c46d5f1d2a9f8c695616a20de4
aodavid <ao.diwei@newdimchina.com>
2016-10-28 18:07:08 +0800

    x

--- api/python/data/main_2.py
+++ api/python/data/main_2.py
@@ -7,6 +7,7 @@ __mtime__ = "2016/7/23"
 __purpose__ = 
 """
 import sys
+import platform
 sys.path.append("..")
 import env
 
@@ -16,5 +17,11 @@ from data_storage.data_store import DataStorage
 if __name__ == "__main__":
     data_mgr = DataStorage("manual_operation")
     # data_mgr.store_data_from_disk()
-    path = "/Users/David/Documents/唇腭裂q36.920150101-20151231_u8.csv"
+    plat = platform.system()
+    if 'Windows' in plat:
+        path = u"D:\MedStudy\source\更新后临床决策数据2015年有改动\更新后临床决策数据2015年有改动\临床决策系统数据CSV版\唇腭裂q36.920150101-20151231.csv"
+    elif "Darwin" in plat:
+        path = "/Users/David/Documents/唇腭裂q36.920150101-20151231_u8.csv"
+    else:
+        pass
     data_mgr.parse_csv_file(path=path)
