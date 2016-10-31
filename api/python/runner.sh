6568da28d47b62dde6b25e46751cffef7c601b12
aodiwei <1qaz2wsx>
2016-11-01 01:09:34 +0800

    runner

--- api/python/runner.sh
+++ api/python/runner.sh
@@ -4,8 +4,8 @@ nohup python -m user.main --port=8002 > /dev/null 2>&1 &
 nohup python -m user.main --port=8003 > /dev/null 2>&1 &
 nohup python -m user.main --port=8004 > /dev/null 2>&1 &
 
-nohup python -m user.main --port=8005 > /dev/null 2>&1 &
-nohup python -m user.main --port=8006 > /dev/null 2>&1 &
-nohup python -m user.main --port=8007 > /dev/null 2>&1 &
-nohup python -m user.main --port=8008 > /dev/null 2>&1 &
-nohup python -m user.main --port=8009 > /dev/null 2>&1 &
+nohup python -m data.main --port=8005 > /dev/null 2>&1 &
+nohup python -m data.main --port=8006 > /dev/null 2>&1 &
+nohup python -m data.main --port=8007 > /dev/null 2>&1 &
+nohup python -m data.main --port=8008 > /dev/null 2>&1 &
+nohup python -m data.main --port=8009 > /dev/null 2>&1 &
