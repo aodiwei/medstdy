1c21f9ceaea8def53578bdd6da035cb10fdaf59e
aodavid <ao.diwei@newdimchina.com>
2016-11-01 16:07:22 +0800

    port

--- api/python/runner.sh
+++ api/python/runner.sh
@@ -4,8 +4,9 @@ nohup python -m user.main --port=8002 > /dev/null 2>&1 &
 nohup python -m user.main --port=8003 > /dev/null 2>&1 &
 nohup python -m user.main --port=8004 > /dev/null 2>&1 &
 
-nohup python -m data.main --port=8005 > /dev/null 2>&1 &
+
 nohup python -m data.main --port=8006 > /dev/null 2>&1 &
 nohup python -m data.main --port=8007 > /dev/null 2>&1 &
 nohup python -m data.main --port=8008 > /dev/null 2>&1 &
-nohup python -m data.main --port=8009 > /dev/null 2>&1 &
+nohup python -m data.main --port=8010 > /dev/null 2>&1 &
+nohup python -m data.main --port=8011 > /dev/null 2>&1 &
